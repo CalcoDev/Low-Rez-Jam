23754022d43bd4032f7a553a5ff130585caf03cc
Calco Dev <calcopoddev@gmail.com>
2022-08-09 23:27:13 +0300

    Fixed movement: should work the same, regardless of FPS and stuff.

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -8,10 +8,15 @@ var curr_pos = Vector2.ZERO
 func _ready():
 	curr_pos = position
 
+# Limited FPS to 240 (4 * 60)
 func _process(delta):
 	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input.y = -Input.get_action_strength("ui_up") + Input.get_action_strength("ui_down")
+	
+	# go pew pew
 
+# Limited FPS to 60 (1 * 60)
+func _physics_process(delta):
 	var force = input.normalized() * speed * delta;
 	curr_pos += force
 	
